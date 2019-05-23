0444ab62f3d06dd0a9633c5e94e8ffd88eb52d24
Anna Kremláčková <annakremlackova@Anna.local>
2019-05-23 23:55:25 +0200

    added doplnovacka.py

---
+++ doplnovacka.py
@@ -0,0 +1,39 @@
+"""
+doplnovacka.py 
+Moje kamarádka učí soukromě němčinu. Potřebuje pro klienty připravit texty, 
+které se časem naučí nazpaměň (jedná se o právní předpisy). 
+Proto jim připravuje stále stejný text, ve kterém vynechá nejprve každé 
+5. slovo, později každé 4. slovo atd., až se text naučí zpaměti. 
+Výstupem bude nový soubor. Pracujte se souborem lorem.py.
+Vytvořte pro ni program, jehož vstupem bude textový soubor a informace, 
+kolikátý znak se má zaměnit.
+"""
+
+with open("lorem.txt") as f:
+	text = f.read()
+f.closed
+text = text.replace("  ", " ")
+text = text.replace("\n", "# ")
+
+seznam = []
+seznam = text.split(" ")
+#print(seznam)
+vysledek = []
+interpunkce = ".,?!-;\""
+
+for i in range(0, len(seznam)):
+	slovo = seznam[i]
+	slovoUpraveno = ""
+	if ((i + 1) % 5 == 0):
+		for pismeno in slovo:
+			if pismeno not in interpunkce:
+				slovoUpraveno += "*"
+			else:
+				slovoUpraveno += pismeno
+		vysledek.append(slovoUpraveno)
+	else:
+		vysledek.append(slovo)
+
+s = " ".join(vysledek)
+s = s.replace("#", "\n")
+print(s)
